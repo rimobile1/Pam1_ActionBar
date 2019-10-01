f8fc27f355a6747ee43734af935e77f2155eccbf
rimobile1 <icaro.mo1234@gmail.com>
2019-09-30 22:29:12 -0300

    codificaçao actionbaractivity

--- app/src/main/java/com/example/etec_actionbar/ActionBarActivity.java
+++ app/src/main/java/com/example/etec_actionbar/ActionBarActivity.java
@@ -2,6 +2,10 @@ package com.example.etec_actionbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActionBarActivity extends AppCompatActivity {
 
@@ -9,5 +13,45 @@ public class ActionBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_wifi:
+                Toast.makeText(this, "Wi-Fi acionado!", Toast.LENGTH_LONG).show();
+                return  true;
+
+            case R.id.action_bluetooth:
+                Toast.makeText(this, "Bluetooth acionado!", Toast.LENGTH_LONG).show();
+                return  true;
+
+            case R.id.action_sobre:
+                Toast.makeText(this, "Sobre acionado!", Toast.LENGTH_LONG).show();
+                return  true;
+
+            case R.id.action_sair:
+               finish();
+                return  true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+
+
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater() .inflate(R.menu.actions,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
